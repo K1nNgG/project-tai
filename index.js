cfed43b56c3852f78c140c0b2b80e605d96c1052
K1nNgG <vasyl.korol.61@gmail.com>
2026-02-03 08:38:07 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function zmien1() {
+    console.log('1')
+    document.getElementById("mFunkcja").style.display = "flex";
+}
+
+function zmien2() {
+    console.log('2')
+}
+
+function zPowrotem1() {
+    console.log('3')
+    document.getElementById("mFunkcja").style.display = "none";
+     document.getElementById("mFunkcja").style.boxSizing = "border-box";
+}
+
+function zPowrotem2() {
+    console.log('4')
+}
