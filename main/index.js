58add0c3f9d59d6298883029a18ab06b94e05937
K1nNgG <vasyl.korol.61@gmail.com>
2026-03-10 10:23:49 +0100

    czwarty 10.03

--- main/index.js
+++ main/index.js
@@ -1,17 +1,21 @@
+// Zmienia sekcję Matematyka
 function zmien1() {
     document.getElementById("mFunkcja").style.display = "flex";
 }
 
+// Zmienia sekcję Ekonomia
 function zmien2() {
-	document.getElementById("eFunkcja").style.display = "flex";
+    document.getElementById("eFunkcja").style.display = "flex";
 }
 
+// Ukrywa zmienioną sekcję Matematyka
 function zPowrotem1() {
     document.getElementById("mFunkcja").style.display = "none";
     document.getElementById("mFunkcja").style.boxSizing = "border-box";
 }
 
+// Ukrywa zmienioną sekcję Ekonomia
 function zPowrotem2() {
-	document.getElementById("eFunkcja").style.display = "none";
+    document.getElementById("eFunkcja").style.display = "none";
     document.getElementById("eFunkcja").style.boxSizing = "border-box";
 }
